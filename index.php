11ea1c70d3c44a2b36f177c7429a78fa3be17f1b
Litanovitasari <litanovitasari@student.untan.ac.id>
2021-02-18 17:42:49 +0700

    praktikum 02 membuat objek dan class

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+
+include ('Mahasiswa.php');
+
+$lita = new Mahasiswa ();
+$devi = new Mahasiswa ();
+$siauling = new Mahasiswa ()
+
+$lita->nim = 58;
+$devi->nim = 57;
+$siauling->nim = 56;
+
+$lita->tampilkanAngkatan();
+
+?>
