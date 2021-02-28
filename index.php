61a77db9f7d7bfbbc2c4859ba927943f6e91900f
Litanovitasari <78336740+Litanovitasari@users.noreply.github.com>
2021-02-28 18:20:39 +0700

    Update index.php

--- index.php
+++ index.php
@@ -6,9 +6,25 @@ $lita = new Mahasiswa ();
 $devi = new Mahasiswa ();
 $siauling = new Mahasiswa ()
 
-$lita->nim = 58;
-$devi->nim = 57;
-$siauling->nim = 56;
+$lita = new Mahasiswa ();
+$lita->nim ='H1101191058';
+$lita->nama = 'Lita Novitasari';
+$lita->tanggal_lahir = '2001-11-09';
+$lita->jenis_kelamin = 'Wanita';
+
+
+$devi = new Mahasiswa ();
+$devi->nim = 'H1101191057';
+$devi->nama = 'Devi Indah';
+$devi->tanggal_lahir = '2001-01-01';
+$devi->jenis_kelamin = 'Wanita';
+
+
+$siauling = new Mahasiswa ()
+$siauling->nim = 'H1101191024';
+$siauling->nama = 'Siauling';
+$siauling->tanggal_lahir = '2001-12-12';
+$siauling->jenis_kelamin = 'Wanita'
 
 $lita->tampilkanAngkatan();
 
